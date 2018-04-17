45c5530a9d20945ccb331c8a62b896182dabda63
5donuts <charliegerman32@gmail.com>
2018-04-17 19:38:27 -0400

    rewrote the DNS server

--- dnsserver.py
+++ dnsserver.py
@@ -3,170 +3,152 @@ Charles German & Gabe Lake
 DNS Server
 """
 from socket import *
-from copy import deepcopy
 
-# global variables
+# ip addr of the webserver
 WEBSERVER_IP_ADDR = '127.0.0.1'
 
+
 # set up UDP server
-serverPort = 53
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
+def setup():
+    port = 53
+    ip_addr = '127.0.0.1'
+    sock = socket(AF_INET, SOCK_DGRAM)
+    sock.bind((ip_addr, port))
+
+    return sock
 
 
-# serverSocket.listen(1)
+# get the flags from the DNS request for the response
+def getflags(flag_bytes):
+    byte1 = bytes(flag_bytes[:1])
+    # byte2 = bytes(flag_bytes[1:])
+
+    qr = '1'  # this is a response
+
+    opcode = ''
+    for bit in range(1, 5):
+        opcode += str(ord(byte1) & (1 << bit))
+
+    aa = '1'  # this is an authoritative answer
+    tc = '0'  # this is not truncated
+    rd = '0'  # recursion is not supported
+    ra = '0'  # recursion is not supported
+    z = '000'  # reserved
+    rcode = '0000'  # assume all queries are successful
+
+    # build response bytes
+    rbyte1 = int(qr + opcode + aa + tc + rd, 2).to_bytes(1, byteorder='big')
+    rbyte2 = int(ra + z + rcode, 2).to_bytes(1, byteorder='big')
+
+    return rbyte1 + rbyte2
 
 
-# turn an ugly bit string into a nicely formatted bit string (Jorn's code)
-# from https://joernhees.de/blog/2010/09/21/how-to-convert-hex-strings-to-binary-ascii-strings-in-python-incl-8bit-space/
-def binary(bitstr):
-    return ' '.join(reversed(
-        [i + j for i, j in zip(*[["{0:04b}".format(int(c, 16)) for c in reversed('0' + x)][n::2] for n in [1, 0]])]))
+def get_domain_name(domain_name_bytes):
+    # get the domain name
+    expected_length = 0
+    part = ''
+    domain = []
+    length_mode = True
+
+    for byte in domain_name_bytes:
+        if length_mode:
+            expected_length = byte
+            length_mode = False
+        else:
+            if byte == 0:
+                domain.append(part)
+                break
+
+            part += chr(byte)
+            if len(part) == expected_length:
+                domain.append(part + '.')
+                part = ''
+                length_mode = True
+
+    return ''.join(domain)
 
 
-# turn hexadecimal data into a bit string
-def hex2bitstr(hex_val):
-    return bin(int(hex_val, 16))[2:].zfill(8)
+# turn the WEBSERVER_IP_ADDR into a byte string
+def get_rdata():
+    parts = WEBSERVER_IP_ADDR.split('.')
+    addr_bytes = b''
+    for part in parts:
+        addr_bytes += bytes([int(part)])
 
-# parse the DNS header and return a list containing all elements of it
-# this method assumes that header_bitstr has been formatted using Jorn's code
-def parseHeader(header_bitstr):
-    # get the octets
-    octets = header_bitstr.split(" ")
-    ID = octets[0] + octets[1]
-
-    # parse the flags
-    flags = octets[2] + octets[3]
-    QR = int(flags[:1], 2)
-    Opcode = int(flags[1:4], 2)
-    AA = int(flags[4:5], 2)
-    TC = int(flags[5:6], 2)
-    RD = int(flags[6:7], 2)
-    RA = int(flags[7:8], 2)
-    Z = int(flags[8:11], 2)
-    RCODE = int(flags[11:], 2)
-
-    # rest of the header
-    QDCOUNT = int(octets[4], 2)
-    ANCOUNT = int(octets[5], 2)
-    NSCOUNT = int(octets[6], 2)
-    ARCOUNT = int(octets[7], 2)
-
-    # build & return the list
-    return [ID, QR, Opcode, AA, TC, RD, RA, Z, RCODE, QDCOUNT, ANCOUNT, NSCOUNT, ARCOUNT]
+    return addr_bytes
 
 
-# generate the DNS response header using the parsed header
-def genResponseHeader(parsed_header):
-    # copy the header for the response
-    response_list = deepcopy(parsed_header)
+def build_body(record_type, record_class):
+    offset = b'\xc0\x0c'  # offset of 12
+    record_ttl = (3600).to_bytes(4, byteorder='big')  # arbitrary value
+    rdlength = b'\x00\x04'  # 4 bytes in the record
+    rdata = get_rdata()
 
-    # set QR to '1'
-    response_list[1] = '1'
-
-    # set ANCOUNT to '0x01'
-    response_list[10] = '0' * 15 + '1'
-
-    # build the response
-    return ''.join(response_list)
-
-# parse the question section of the DNS packet and return a list containing elements
-# this method assumes that quest_bitstr has been formatted using Jorn's code
-def parseQuestion(quest_bitstr):
-    # get the octets
-    octets = quest_bitstr.split(" ")
-
-    idx = 1
-    questions = []
-    lenOct = octets[0]
-
-    # TODO since we don't actually need the questions,
-    # need to modify this so it only finds out how long
-    # QNAME is
-
-    # read until the 0 octet (null label) is reached
-    while lenOct != '0' * 8:
-        length = int(lenOct, 2)
-        question = ""
-        for i in range(0, length):
-            question += octets[idx]
-            idx += 1
-        questions.append(question)
-        lenOct = octets[idx]
-        idx += 1
-
-    # assemble QNAME with labels
-    QNAME_list = []
-    for i in range(0, idx):
-        QNAME_list.append(octets[i])
-    QNAME = ''.join(QNAME_list)
-
-    # get qtype
-    QTYPE = octets[idx] + octets[idx + 1]
-    idx += 2
-
-    # get qclass
-    QCLASS = octets[idx] + octets[idx + 1]
-
-    # return questions, qtype, qclass
-    return [QNAME, QTYPE, QCLASS]
+    return offset + record_type + record_class + record_ttl + rdlength + rdata
 
 
-# build the DNS answer
-# all of the response values are going to be hard-coded
-def genAnswer(parsed_question):
-    # same as QNAME
-    NAME = parsed_question[0]
+# build the question for the response
+def build_question(domain_name_str):
+    domain_bytes = b''
 
-    # set TYPE to 0x01
-    TYPE = '0' * 15 + '1'
+    # build bytes for the domain name
+    for part in domain_name_str.split('.'):
+        domain_bytes += bytes([len(part)])
 
-    # set CLASS to 0x01
-    CLASS = '0' * 15 + '1'
+        for char in part:
+            domain_bytes += ord(char).to_bytes(1, byteorder='big')
 
-    # set TTL to 600
-    TTL = '0' * 22 + '1001011000'
+    # record type information
+    record_type = b'\x00\x01'  # for A record type
+    class_type = b'\x00\x01'  # for IN class type
 
-    # the IP address to return
-    octet_vals = WEBSERVER_IP_ADDR.split('.')
-    octets = []
-    for val in octet_vals:
-        octets.append(int(val))
-    RDATA = ''.join(octets)
-
-    # combine values into a single bit string
-    return NAME + TYPE + CLASS + TTL + RDATA
+    return domain_bytes + record_type + class_type
 
 
-# Construct the final response packet
-def genResponse(response_header, response_answer):
-    return response_header + response_answer
+# build the response header
+def build_header(header_data):
+    # transaction id
+    transaction_id = header_data[:2]  # first 2 bytes
+
+    # flags
+    flags = getflags(header_data[2:4])
+
+    # question count
+    qdcount = b'\x00\x01'
+
+    # answer count
+    ancount = b'\x00\x01'
+
+    # nameserver count
+    nscount = b'\x00\x00'
+
+    # additional records
+    arcount = b'\x00\x00'
+
+    return transaction_id + flags + qdcount + ancount + nscount + arcount
 
 
-# listen for connections until SIGTERM is received
-print("DNS server started. Use ^C to exit")
-while True:
-    # get DNS request
-    request, addr = serverSocket.recvfrom(4096)
-    bin_val = hex2bitstr(request)
-    formatted = binary(bin_val).split(" ")
+# build the response
+def build_response(request):
+    name = get_domain_name(request[12:-4])
+    print("Processing request for: " + name)
 
-    # build response header
-    list = []
-    for i in range(0, 8):
-        list.append(formatted[i])
-    header_bitstr = ' '.join(list)
-    header = parseHeader(header_bitstr)
-    response_header = genResponseHeader(header)
+    header = build_header(request[:12])
+    question = build_question(name)
+    body = build_body(question[-4:-2], question[-2:])
 
-    # build answers
-    list = []
-    for i in range(8, len(formatted)):
-        list.append(formatted[i])
-    quest_bitstr = ' '.join(list)
-    question = parseQuestion(quest_bitstr)
-    answer = genAnswer(question)
+    return header + question + body
 
-    # build & send response
-    response = genResponse(response_header, answer)
-    serverSocket.sendto(response, addr)
+
+if __name__ == "__main__":
+    sock = setup()
+
+    # listen for connections until SIGTERM is received
+    print("DNS server started. Use ^C to exit")
+    while True:
+        # get DNS request, 512 byte packets use UDP
+        request, addr = sock.recvfrom(512)
+
+        # build & send response
+        response = build_response(request)
+        sock.sendto(response, addr)
