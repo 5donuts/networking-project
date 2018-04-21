957e358ea10d502e0926913118f39458041d94b6
5donuts <charliegerman32@gmail.com>
2018-04-21 19:57:55 -0400

    first version of soundclient.py

--- soundclient.py
+++ soundclient.py
@@ -1,7 +1,25 @@
-from socket import * 
-serverName = '127.0.0.1'
-serverPort = 4000
-clientSocket = socket(AF_INET, SOCK_STREAM) 
-clientSocket.connect((serverName,serverPort))
-clientSocket.send(bytes("101001101001001010100101010101010010101001010100101010101001010010101010010100011"*1000,'utf-8'))
-clientSocket.close()
+#!/usr/bin/env python3
+
+from socket import *
+
+
+# connect to the sound server
+def connect():
+    server_name = '127.0.0.1'
+    server_port = 4000
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.connect((server_name, server_port))
+
+    return sock
+
+
+if __name__ == "__main__":
+    # establish connection
+    sock = connect()
+
+    # send message
+    message = raw_input("Message to send: ")
+    sock.send(message)
+
+    # close connection
+    sock.close()
