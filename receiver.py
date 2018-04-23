43c63716606d50d151bec1bbfeca07848dc6f4c6
5donuts <charliegerman32@gmail.com>
2018-04-23 01:14:21 -0400

    added code to save transmissions to/read transmissions from .wav files

--- receiver.py
+++ receiver.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
-from transmitter import INTER_TRANSMISSION_PAUSE, AUDIO_SAMPLE_RATE
+from transmitter import INTER_TRANSMISSION_PAUSE, TONE_DURATION, AUDIO_SAMPLE_RATE, AUDIO_SAMPLES_PER_TONE, get_hash
 from rtlsdr import RtlSdr
 import numpy as np
 import scipy.signal as signal
+from scipy.io.wavfile import read
+from bitstring import BitArray
+import hashlib
+from math import ceil
 
 # constants
 STATION_FREQ = int(87.7e6)  # in Hz
-OFFSET_FREQ = 250000  # offset to capture at (use offset to avoid DC spike)
+OFFSET_FREQ = 250000  # offset to capture at, see https://witestlab.poly.edu/blog/capture-and-decode-fm-radio/
 CENTER_FREQ = STATION_FREQ - OFFSET_FREQ
 RADIO_SAMPLE_RATE = int(1140000)
-SAMPLES_PER_SECOND = 1131227  # approx. number of radio samples per 1 second of audio
 TRANSMISSION_AUDIO_SAMPLE_RATE = AUDIO_SAMPLE_RATE
 
 
@@ -50,7 +53,7 @@ def filter_and_downsample(samples):
 
 
 # demodulate using a polar discriminator
-def demodulate(samples):
+def apply_polar_discriminator(samples):
     print("Applying polar discriminator...", end='', flush=True)
     y = samples[1:] * np.conj(samples[:-1])
     x = np.angle(y)
@@ -58,8 +61,8 @@ def demodulate(samples):
     return x
 
 
-# de-emphasis filter
-def de_emphasis_filter(demodulated_samples, sample_rate):
+# apply de-emphasis filter
+def apply_de_emphasis_filter(demodulated_samples, sample_rate):
     print("Applying de-emphasis filter...", end='', flush=True)
     d = sample_rate * 75e-6  # number of samples to hit -3dB point
     x = np.exp(-1 / d)  # decay between each sample
@@ -91,7 +94,7 @@ def get_radio_samples(n):
     # setup sdr
     sdr = setup()
 
-    # read samples
+    # get samples from radio
     print("Sampling radio...", end='', flush=True)
     read_size = 8192  # recommendation from https://github.com/roger-/pyrtlsdr/issues/56
     num_read = 0
@@ -127,10 +130,10 @@ def get_audio_samples(n):
     samples, sample_rate = filter_and_downsample(samples)
 
     # demodulate using a polar discriminator
-    demodulated_samples = demodulate(samples)
+    demodulated_samples = apply_polar_discriminator(samples)
 
     # apply the de-emphasis filter
-    demodulated_samples = de_emphasis_filter(demodulated_samples, sample_rate)
+    demodulated_samples = apply_de_emphasis_filter(demodulated_samples, sample_rate)
 
     # decimate again to focus on mono part of audio
     mono_audio, mono_sample_rate = get_mono(demodulated_samples, sample_rate)
@@ -142,7 +145,8 @@ def get_audio_samples(n):
     mono_audio = signal.resample(mono_audio, int(num_samples))
     print("done")
 
-    return mono_audio, TRANSMISSION_AUDIO_SAMPLE_RATE
+    # return mono_audio, TRANSMISSION_AUDIO_SAMPLE_RATE
+    return mono_audio, mono_sample_rate
 
 
 # save mono audio to a file
@@ -151,6 +155,213 @@ def save_to_file(filename, mono_audio, sample_rate):
     print("Sample rate for " + filename + ": " + str(sample_rate))
 
 
+# load a wav file into a numpy.ndarray object
+def load_wav(filename):
+    rate, data = read(filename)
+    if rate != AUDIO_SAMPLE_RATE:
+        print("WARNING: " + filename + " does not have sample rate of " + str(AUDIO_SAMPLE_RATE) + "Hz")
+    return data
+
+
+# take audio data and build a list of tones
+def get_tones_from_audio(audio_data):
+    # TODO handle possibility of audio data left over at the end
+    print("Building tone list...", end='', flush=True)
+
+    # build list of tones
+    tones = list(chunks(audio_data[1:], AUDIO_SAMPLES_PER_TONE))
+
+    # pad last tone with extra 0s if necessary
+    while len(tones[-1]) < 8:
+        tones[-1].append(0)
+
+    print("done")
+    return tones
+
+
+# see https://stackoverflow.com/a/312464
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+# determine the average value of a tone, only considering the absolute value of its components
+def average_tone(tone):
+    avg = 0
+    for val in tone:
+        avg += abs(val)
+    avg /= len(tone)
+    return avg
+
+
+# demodulate tones
+def demodulate(tones):
+    data = []
+
+    print("Demodulating audio data...", end='', flush=True)
+    for tone in tones:
+        avg = average_tone(tone)
+        # TODO improve this using TONE_HIGH and TONE_LOW
+        if avg < 1000:
+            data.append(0)
+        else:
+            data.append(1)
+    print("done")
+
+    return data
+
+
+# detect and separate transmissions
+def separate_transmissions(demodulated_data):
+    transmissions = []
+    min_tones_between_transmissions = ceil(INTER_TRANSMISSION_PAUSE / TONE_DURATION)
+
+    print("Separating transmissions...", end='', flush=True)
+    run_length = 0
+    prev_start = 0
+    idx = 0
+    for bit in demodulated_data:
+        idx += 1
+
+        # find runs of zeroes
+        if bit == 0:
+            run_length += 1
+        else:
+            run_length = 0
+
+        # separate transmissions
+        if run_length == min_tones_between_transmissions:
+            run_length = 0
+            transmissions.append(demodulated_data[prev_start:idx - min_tones_between_transmissions])
+            prev_start = idx - min_tones_between_transmissions
+    print("done")
+
+    return transmissions
+
+
+# rebuild the packet from the data
+def rebuild_packet(data):
+    packet = b''
+
+    # preamble
+    packet += BitArray(data[:32]).bytes
+
+    # source ip
+    packet += BitArray(data[32:64]).bytes
+
+    # transmitter ip
+    packet += BitArray(data[64:96]).bytes
+
+    # sequence number
+    packet += BitArray(data[96:104]).bytes
+
+    # data length
+    packet += BitArray(data[104:120]).bytes
+
+    # reserved
+    packet += BitArray(data[120:128]).bytes
+
+    # checksum
+    packet += BitArray(data[128:160]).bytes
+
+    # data
+    packet += BitArray(data[160:]).bytes
+
+    return packet
+
+
+# get information from packet
+def get_packet_info(packet):
+    info = {}
+
+    # preamble is 32 bits (4 bytes)
+    # source ip (32 bits)
+    source_ip_bytes = packet[4:8]
+    ip = []
+    for byte in source_ip_bytes:
+        ip.append(int(byte))
+    info['source_ip'] = '.'.join(ip)
+
+    # transmitter ip (32 bits)
+    transmitter_ip_bytes = packet[8:12]
+    ip = []
+    for byte in transmitter_ip_bytes:
+        ip.append(int(byte))
+    info['transmitter_ip'] = '.'.join(ip)
+
+    # sequence number (8 bits)
+    info['sn'] = int(packet[12:13])
+
+    # data length (16 bits)
+    info['data_length'] = BitArray(packet[13:15]).int
+
+    # reserved is 8 bits (1 byte)
+    # checksum (32 bits)
+    checksum_bytes = packet[16:20]
+    checksum = []
+    for byte in checksum_bytes:
+        int_val = BitArray(byte).int
+        checksum.append(chr(int_val))
+    info['checksum'] = ''.join(checksum)
+
+    # data
+    info['data'] = packet[20:]
+
+    return info
+
+
+# display packet information
+# if data is not None, use data instead of info_dict['data']
+def display_packet_info(info_dict, data=None):
+    print("Received packet: ")
+    print("Source: " + source_ip + "\tTransmitter: " + trans_ip)
+    print("SN: " + str(sn) + "\tLength: " + str(length))
+    print("Checksum: " + checksum)
+    if data is not None:
+        show_data = data
+    else:
+        show_data = info_dict['data']
+    print("Message: " + show_data)
+
+
 if __name__ == "__main__":
-    audio, sample_rate = get_audio_samples(SAMPLES_PER_SECOND * 10)
-    # save_to_file("output.raw", audio, sample_rate)
+    # TODO implement getting audio from transmitter
+
+    # for testing purposes, load audio data from a wav file
+    audio_data = load_wav("sample_transmission_data.wav")
+
+    # get tones from the audio data
+    tones = get_tones_from_audio(audio_data)
+
+    # demodulate data
+    demodulated_data = demodulate(tones)
+
+    # separate transmissions
+    transmissions = separate_transmissions(demodulated_data)
+
+    # display contents of each transmission
+    i = 0
+    for transmission in transmissions:
+        i += 1
+        print("Processing transmission " + str(i) + " of " + str(len(transmissions)))
+
+        # build packet from demodulated data
+        packet = rebuild_packet(demodulated_data)
+
+        # get packet info
+        info_dict = get_packet_info(packet)
+
+        # validate message checksum
+        checksum = get_hash(info_dict['data'])
+        if checksum != info_dict['checksum']:
+            print("WARNING: message checksum does not match calculated value; data is corrupted")
+            # display the raw data
+            display_packet_info(info_dict)
+        else:
+            # TODO decrypt the message
+            message = decrypt(info_dict['data'], '')
+
+            # display the decrypted message
+            display_packet_info(info_dict, data=message)
+
--- transmitter.py
+++ transmitter.py
@@ -6,6 +6,8 @@ from socket import *
 import hashlib
 from time import sleep
 from bitstring import BitArray
+from scipy.io.wavfile import write
+from math import ceil
 
 # global variables
 TONE_DURATION = 0.05  # seconds (if this is changed, make sure to modify SAMPLES_PER_TONE)
@@ -13,7 +15,8 @@ AUDIO_SAMPLES_PER_TONE = 6616  # at 44100Hz TODO find a way to programmatically 
 TONE_HIGH = 5000  # Hz
 TONE_LOW = 0  # Hz
 AUDIO_SAMPLE_RATE = 44100  # Hz
-INTER_TRANSMISSION_PAUSE = 1  # seconds
+INTER_TRANSMISSION_PAUSE = 5  # seconds
+PACKET_REPETITIONS = 5  # number of times each packet will be transmitted
 TRANSMITTER_ADDR = '192.168.0.1'  # TODO find a way to programmatically determine this
 
 
@@ -29,7 +32,7 @@ def setup():
 
 
 # build the list of tones representing a modulated packet
-def build_transmission_data(packet):
+def build_transmission(packet):
     # initialize list
     transmission_data = []
 
@@ -46,45 +49,30 @@ def build_transmission_data(packet):
     return transmission_data
 
 
+# build the transmission data for all packets
+def build_multiple_transmissions(packets):
+    full_transmission_data = []
+    print("Building transmission data...", end='', flush=True)
+    for packet in packets:
+        # build data for single packet
+        full_transmission_data.append(build_transmission(packet))
+    print("done")
+    return full_transmission_data
+
+
 # play the transmission data
 def send_transmission(transmission_data):
     # play all tones in the transmission
-    print("Playing transmission...", end='', flush=True)
     for tone in transmission_data:
         sd.play(tone, AUDIO_SAMPLE_RATE)
         sd.wait()
-
-    print("done")
-
-
-# send each packet the given number of times with a pause between transmissions
-def transmit_packet(packet, repetitions):
-    # build transmission data for all repetitions
-    print("Building transmission data...", end='', flush=True)
-    full_transmission_data = []
-    for i in range(0, repetitions):
-        # build data for single transmission
-        full_transmission_data.append(build_transmission_data(packet))
-
-        # update sequence number
-        packet = packet[:64] + bytes([i]) + packet[69:]
-    print("done")
-
-    # send all transmissions with a pause between transmissions
-    for i in range(0, repetitions):
-        send_transmission(full_transmission_data[i])
-        sleep(INTER_TRANSMISSION_PAUSE)
-
-
-# play a tone using a given audio stream
-def play_tone(tone, audio_stream):
-    audio_stream.write(tone)
 
 
 # generate a tone of the given duration (in seconds) at the given frequency
 def gen_tone(tone_duration, frequency):
     duration = tone_duration * 3  # this makes a duration of 1 approx. equal to 1 second
-    tone = (np.sin(2 * np.pi * np.arange(AUDIO_SAMPLE_RATE * duration) * frequency / AUDIO_SAMPLE_RATE)).astype(np.float32)
+    tone = (np.sin(2 * np.pi * np.arange(AUDIO_SAMPLE_RATE * duration) * frequency / AUDIO_SAMPLE_RATE)).astype(
+        np.float32)
     return tone
 
 
@@ -111,6 +99,9 @@ def get_ip_addr_bytes(ip_addr):
 def build_packet(source_ip, transmitter_ip, sequence_number, checksum, data):
     packet = b''
 
+    # preamble
+    packet += b'\x55' * 32  # alternating pattern of 1s and 0s
+
     # source ip
     packet += get_ip_addr_bytes(source_ip)
 
@@ -118,7 +109,7 @@ def build_packet(source_ip, transmitter_ip, sequence_number, checksum, data):
     packet += get_ip_addr_bytes(transmitter_ip)
 
     # sequence number
-    packet += bytes([int(sequence_number)])
+    packet += sequence_number.to_bytes(1, byteorder='big')
 
     # data length
     packet += len(data).to_bytes(2, byteorder='big')
@@ -138,11 +129,49 @@ def build_packet(source_ip, transmitter_ip, sequence_number, checksum, data):
     return packet
 
 
+# save a numpy array as a wave file
+def save_wav(filename, audio_data):
+    write(filename, AUDIO_SAMPLE_RATE, audio_data)
+    print("Saved file " + filename + " successfully.")
+
+
+# save transmission data into a wave file
+def save_transmission_data(transmission_data):
+    print("Building data for wave file...", end='', flush=True)
+    # generate tone data for pauses between repetitions
+    num_tones = ceil(INTER_TRANSMISSION_PAUSE / TONE_DURATION)
+    pause_tones = []
+    for i in range(0, num_tones):
+        pause_tones.append(gen_tone(TONE_DURATION, TONE_LOW))
+
+    # build numpy array to store tones to be saved
+    wav_data = np.array(0, ndmin=1)  # need initial value for array
+    i = 0
+    for transmission in transmission_data:
+        # store all tones for transmission
+        for tone in transmission:
+            wav_data = np.concatenate([wav_data, tone])
+
+            # remove leading 0 of wav_data
+            if i == 0:
+                wav_data = wav_data[1:]
+
+        # store empty tones between transmission
+        for tone in pause_tones:
+            wav_data = np.concatenate([wav_data, tone])
+
+        i += 1
+    print("done")
+
+    # save the generated audio to a file
+    save_wav("multi_transmission.wav", wav_data)
+
+
 if __name__ == "__main__":
     sock = setup()
 
     # listen for connections until SIGTERM is received
-    print("Sound server started. Use ^C to exit")
+    print("Transmitter started. Use ^C to exit")
 
     while True:
         # establish connection with client
@@ -153,9 +182,28 @@ if __name__ == "__main__":
         print("Processing message: ", end='')
         print(message)
 
-        # send the packet over the 'wire' 30 times
-        transmit_packet(message, 30)
+        # build the packets
+        packets = []
+        print("Building packets...", end='', flush=True)
+        for i in range(0, PACKET_REPETITIONS):
+            packets.append(build_packet(source_addr[0], TRANSMITTER_ADDR, i + 1, get_hash(message), message))
+        print("done")
+
+        # build transmission data for packets
+        transmission_data = build_multiple_transmissions(packets)
+
+        # transmit all data
+        # i = 1
+        # for transmission in transmission_data:
+        #     print("Transmission " + str(i) + " of " + str(PACKET_REPETITIONS) + "...", end='', flush=True)
+        #     send_transmission(transmission)
+        #     print("done")
+        #     sleep(INTER_TRANSMISSION_PAUSE)
+        #     i += 1
+        # del i
+
+        # for testing purposes, save the audio data to a wave file
+        save_transmission_data(transmission_data)
 
         # close the connection
         connection.close()
-
